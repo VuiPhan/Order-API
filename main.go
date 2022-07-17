34b9b4a922638c0e922f07f725f8188a0af5e52a
VuiPhan <phanvui286@gmail.com>
2022-07-17 22:59:38 +0700

    change to local3000

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/VuiPhan/web-service-gin/db_client"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func main() {
 	fmt.Println("heheee")
-	db_client.InitialConnection()
+	//db_client.InitialConnection()
 	router := gin.Default()
 	//router.Use(middleware.CORSMiddleware())
 	//router.POST("/create-order", controllers.CreateOrder)
